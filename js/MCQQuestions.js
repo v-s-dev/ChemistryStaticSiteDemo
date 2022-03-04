8753904312cf90cce59be7701fdb3f347624c308
v <vinit.shah.dev@gmail.com>
2022-03-04 21:08:53 +0530

    Added quiz taking feature

---
+++ js/MCQQuestions.js
@@ -0,0 +1,80 @@
+ /* Mcq Question template
+
+'id':{
+    'question':'',
+    'image_url':null,
+    'answer':'',
+    'jumble':true,
+    'options':[
+        '',
+        '',
+        ''
+    ],
+    'option_remarks':[
+        '',
+        '',
+        ''
+    ],
+    'hints':[
+        '',
+        '',
+        ''
+    ]
+},
+
+
+
+
+
+*/
+
+mcqQuestions = {
+    '1':{
+        'question':'What are ligands ?',
+        'image_url':null,
+        'answer':'Small Molecules or ions that share electrons from lone pair',
+        'jumble':true,
+        'options':[
+            'Metals that recieve electrons',
+            'Small Molecules or ions that share electrons from lone pair',
+            ''
+        ],
+        'option_remarks':[
+            'remark 1',
+            'remark 2',
+            ''
+        ],
+        'hints':[
+            'hint 1',
+            'hint 2',
+            'hint 3'
+        ]
+    },
+
+
+    '2':{
+        'question':'Electrons are donated by ___________',
+        'image_url':null,
+        'answer':'',
+        'jumble':true,
+        'options':[
+            'Lewis bases',
+            'Lewis acids',
+            ''
+        ],
+        'option_remarks':[
+            'r 1',
+            'r 2',
+            ''
+        ],
+        'hints':[
+            'h 1',
+            'h 2',
+            'a'
+        ]
+    },
+
+
+
+
+}
